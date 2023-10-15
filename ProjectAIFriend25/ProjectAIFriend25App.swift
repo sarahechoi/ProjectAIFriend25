d0623a895df9a25a9ff0a6fec056867a3a463b95
sarahechoi <sc36skate@gmail.com>
2023-10-15 11:27:01 -0700

    Initial Commit

---
+++ ProjectAIFriend25/ProjectAIFriend25App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectAIFriend25App.swift
+//  ProjectAIFriend25
+//
+//  Created by Sarah Choi on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectAIFriend25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

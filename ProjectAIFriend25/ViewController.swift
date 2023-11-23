5ba8914d13a898a4269dcd2217d8c2c2a5bbf7e0
Lauren Jung <laurenjung25@gmail.com>
2023-11-23 11:01:40 -0800

    viewcontroller?

---
+++ ProjectAIFriend25/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ProjectAIFriend25
+//
+//  Created by Lauren Jung on 11/23/23.
+//
+
+import UIKit
+
+class ViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .link
+
+    }
+}

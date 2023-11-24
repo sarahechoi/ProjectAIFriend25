8bab3bd11523c2cce2b95223471df4f782394906
sarahechoi <sc36skate@gmail.com>
2023-11-23 17:18:44 -0800

    pls

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#  Mend!.sh
+#  ProjectAIFriend25
+#
+#  Created by Sarah Choi on 11/23/23.
+#  
+xcrun simctl spawn booted log config --mode "level:off"  --subsystem com.apple.CoreTelephony
---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  ProjectAIFriend25
+#
+#  Created by Sarah Choi on 11/23/23.
+#  

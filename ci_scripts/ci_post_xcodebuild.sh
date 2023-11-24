571333ed712e254621565cdac26824d77b85d03b
sarahechoi <sc36skate@gmail.com>
2023-11-23 17:23:14 -0800

    its not workinnggggg

--- ci_scripts/ci_post_xcodebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#  ci_post_xcodebuild.sh
-#  ProjectAIFriend25
-#
-#  Created by Sarah Choi on 11/23/23.
-#  
--- workpls/mend.sh
+++ workpls/mend.sh
@@ -4,5 +4,6 @@
 #  ProjectAIFriend25
 #
 #  Created by Sarah Choi on 11/23/23.
-#  
+#
+#!/bin/sh —
 xcrun simctl spawn booted log config --mode "level:off"  --subsystem com.apple.CoreTelephony
